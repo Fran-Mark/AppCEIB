fe3f0662e7dcb623891abf2a6146b22b24d3df80
Fran Marquinez <franciscomarquinez@protonmail.com>
2022-01-19 12:34:57 -0300

    Incorporación de monitoreo constante de conexión a internet, funcionando para movil y web. Usé esto para chequear en la inicialización

--- lib/screens/initial_tabs/main_screen.dart
+++ lib/screens/initial_tabs/main_screen.dart
@@ -3,7 +3,6 @@ import 'package:ceib/screens/initial_tabs/events_screen.dart';
 import 'package:ceib/screens/initial_tabs/feed_screen.dart';
 import 'package:ceib/screens/initial_tabs/home_screen.dart';
 import 'package:ceib/screens/initial_tabs/reservations_screen.dart';
-import 'package:ceib/widgets/logout_button.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
--- lib/widgets/logout_button.dart
+++ lib/widgets/logout_button.dart
@@ -1,29 +1,47 @@
+import 'dart:async';
+
 import 'package:ceib/providers/auth_service.dart';
+import 'package:ceib/providers/connectivity.dart';
 import 'package:ceib/widgets/my_alert_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class LogOutButton extends StatelessWidget {
-  const LogOutButton({Key? key}) : super(key: key);
+class LogOutButton extends StatefulWidget {
+  const LogOutButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<LogOutButton> createState() => _LogOutButtonState();
+}
+
+class _LogOutButtonState extends State<LogOutButton> {
+  Color _backgroundColor = const Color.fromRGBO(255, 230, 234, 1);
+
+  Color _textColor = Colors.red;
 
   @override
   Widget build(BuildContext context) {
     final _authService = Provider.of<AuthServices>(context);
+    final _hasConnection =
+        Provider.of<ConnectionStatusSingleton>(context).hasConnection;
+
+    if (_hasConnection) {
+      _backgroundColor = const Color.fromRGBO(255, 230, 234, 1);
+      _textColor = Colors.red;
+    } else {
+      _backgroundColor = Colors.grey[500]!;
+      _textColor = Colors.grey[200]!;
+    }
 
     Future<void> _logOut(AuthServices authService) async {
       await authService.logout();
       Navigator.of(context).pop();
     }
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 11, horizontal: 5),
-      child: TextButton(
-        style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(
-                const Color.fromRGBO(255, 230, 234, 1))),
-        child: const Text('Cerrar sesión'),
-        onPressed: () {
-          showDialog(
+    void _handler() {
+      if (_hasConnection) {
+        showDialog(
             context: context,
             builder: (BuildContext context) {
               return MyAlertDialog(
@@ -31,9 +49,20 @@ class LogOutButton extends StatelessWidget {
                 content: "Seguro que querés cerrar sesión?",
                 handler: () => _logOut(_authService),
               );
-            },
-          );
-        },
+            });
+      }
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 11, horizontal: 5),
+      child: TextButton(
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(_backgroundColor)),
+        onPressed: _handler,
+        child: Text(
+          'Cerrar sesión',
+          style: TextStyle(color: _textColor),
+        ),
       ),
     );
   }
